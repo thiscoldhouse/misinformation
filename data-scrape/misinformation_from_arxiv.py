6c3d7c8223b73520ec4d223c2a0c611759c0752e
aleruiz <alejandro@posiwid.org>
2025-08-01 17:59:13 -0400

    WIP - updating parsing to do PDFs. Changes to graph making

--- data-scrape/misinformation_from_arxiv.py
+++ data-scrape/misinformation_from_arxiv.py
@@ -2,24 +2,33 @@ import requests
 import json
 import bs4
 import time
-import random
+
+# Notes:
+# Script naively assumes that RESULTSFILE is the
+# in-progress save of whichever raw_data it is
+# currently working from.
+# @TODO: Create some metadata structure to avoid
+# this
 
 URL = "http://export.arxiv.org/api/query?search_query=all:misinformation&max_results=3000"
 RESULTSFILE = 'misinformation_arxiv_parsed.json'
 
+
 class ForbiddenException(Exception):
     pass
+
 
 class SkipException(Exception):
     pass
 
-def fetch_data():
+
+def fetch_raw_data_from_arxiv():
     r = requests.get(URL)
     with open('arxiv_response.xml', 'w+') as f:
         f.write(r.text)
 
         
-def load_results():
+def load_in_progress_results_from_file():
     with open(RESULTSFILE, 'r+') as f:
         try:
             return json.loads(f.read())
@@ -28,12 +37,13 @@ def load_results():
             print('Starting from scratch')
             return []
 
+        
 def save_results(results):
     with open(RESULTSFILE, 'w+') as f:
         f.write(json.dumps(results))
     
     
-def load_data():
+def load_data_from_file():
     data = None
     with open('arxiv_response.xml', 'r+') as f:
         data = f.read()
@@ -41,7 +51,7 @@ def load_data():
     return bs4.BeautifulSoup(data, 'xml')
 
 
-def construct_result_for_paper(paper):
+def fetch_and_parse_paper(paper):
     link = paper.find_all('link')[0]
     link = link['href']
     contents = get_paper_contents(link)
@@ -53,6 +63,22 @@ def construct_result_for_paper(paper):
 
 
 def get_paper_contents(link, retry=0):
+    def verify_source_in_pdf(pdf_url):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Windows; Windows x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36'
+        }
+        response = requests.get(url=pdf_url, headers=headers, timeout=120)
+        reader = PyPDF2.PdfReader(io.BytesIO(response.content))
+        num_pages = len(reader.pages)        
+        for page in reader.pages:
+            text = page.extract_text() 
+            res_search = re.search(DOMAIN, text, re.IGNORECASE)
+            if res_search is not None:
+                return True
+        else:
+            return False
+    
+    import pdb; pdb.set_trace()
     link = link.replace('/abs/', '/html/')
     print(link)
     data = requests.get(link)
@@ -61,18 +87,24 @@ def get_paper_contents(link, retry=0):
         if 'arxiv.org' not in link:
             print(f'{link} is not an arxiv.org link. Skipping')
             raise SkipException()
+
+
+        
         if data.status_code != 403:
             print('Skipping. I only know how to handle 403s and 200s')
             raise SkipException()
         
         else:
             if retry == 0:
+                print('Backing off for 60 seconds')
                 time.sleep(60)        
                 return get_paper_contents(link, retry=1)
             if retry == 1:
+                print('Backing off for 90 seconds')
                 time.sleep(90)        
                 return get_paper_contents(link, retry=2)
-            if retry == 2:                
+            if retry == 2:
+                print('Bailing')
                 raise ForbiddenException()
     
     return {
@@ -82,26 +114,22 @@ def get_paper_contents(link, retry=0):
         'html': data.text
     }
 
-
-def main():
-    #fetch_data()
-    data = load_data()
-    results = load_results()
-    print(f'Preloaded results: {len(results)}')
+def parse(data, previous_results=None):
     papers = data.find_all('entry')
-
-    # trim papers for which we already have results from
-    # previous scrape attempt
-    papers = papers[len(results):]
+    if previous_results is not None:
+        # trim papers for which we already have results from
+        # previous scrape attempt
+        papers = papers[len(results):]
+        
     print(f'Total papers to fetch:{len(papers)}')
     try:
         for i, paper in enumerate(papers):
             try:
-                if i % 5 == 0 and i !=0:
+                if i % 5 == 0 and i > 0:
                     print(f'Loading paper {i}. Taking short break first...')
-                    time.sleep(random.randint(2,5))
+                    time.sleep(5)
                 results.append(
-                    construct_result_for_paper(paper)
+                    fetch_and_parse_paper(paper)
                 )
             except SkipException as e:
                 pass
@@ -112,8 +140,20 @@ def main():
         print('Something happened. Saving what we have.')
         pass
 
+    
+def main():
+    # One of these two should be commented out:
+    # -- #
+    #fetch_raw_data_from_arxiv()
+    data = load_data_from_file()
+    # -- #
+    
+    results = load_in_progress_results_from_file()
+    print(f'Preloaded results: {len(results)}')
+    results = parse(data, previous_results=results)
     save_results(results)
 
+    
 if __name__ == '__main__':
     main()
         
--- parse/make_graphs.py
+++ parse/make_graphs.py
@@ -54,36 +54,77 @@ def make_tables():
         
     ))
     
-make_tables()
+
+def make_graph(nodes, edges, fignumber=1):    
+    plt.figure(fignumber)
+    G = nx.DiGraph()
+    for node in nodes:
+        G.add_node(node[:15])
+    for edge in edges:
+        edge = (e[0:15] for e in edge)
+        G.add_edge(*edge)
+
+    G.remove_nodes_from(list(nx.isolates(G)))
+    node_sizes = [100 * G.degree(n) for n in G.nodes()]
+    node_colors = [G.degree(n) for n in G.nodes()]
+
+    pos = nx.spring_layout(G, k=0.15, iterations=20)
+    nx.draw(
+        G, pos,
+        with_labels=True,
+        node_size=node_sizes,
+        node_color=node_colors,
+        cmap=plt.cm.viridis,
+        edge_color='gray',
+        linewidths=0.5,
+        font_size=4
+    )
+        
 
 def make_network_graph():
-    def make_graph(nodes, edges, fignumber=1):    
-        plt.figure(fignumber)
-        G = nx.DiGraph()
-        for node in nodes:
-            G.add_node(node[:15])
-        for edge in edges:
-            edge = (e[0:15] for e in edge)
-            G.add_edge(*edge)
-
-        G.remove_nodes_from(list(nx.isolates(G)))
-        node_sizes = [100 * G.degree(n) for n in G.nodes()]
-        node_colors = [G.degree(n) for n in G.nodes()]
-
-        pos = nx.spring_layout(G, k=0.15, iterations=20)
-        nx.draw(
-            G, pos,
-            with_labels=True,
-            node_size=node_sizes,
-            node_color=node_colors,
-            cmap=plt.cm.viridis,
-            edge_color='gray',
-            linewidths=0.5,
-            font_size=4
-        )
-
     make_graph(nodes, edges, 1)
     make_graph(nodes_with_dave, edges_with_dave, 2)    
     plt.show()
+
+
+def make_graphs_no_dave(degrees=1):
+    tainted_papers = ['mediabiasfactcheck.com']
+    nodes = nodes_with_dave
+    edges = edges_with_dave
+    for i in range(degrees):
+        remove_is = []
+        print(f'Round {i}: tainted papers: {len(tainted_papers)}')
+        for i, edge_pair in enumerate(edges):
+            if edge_pair[0] in tainted_papers:
+                tainted_papers.append(edge_pair[1])
+            elif edge_pair[1] in tainted_papers:
+                tainted_papers.append(edge_pair[0])
+            else:
+                continue
+
+            remove_is.append(i)
+        for i in sorted(remove_is, key=lambda i: i * -1):
+            # order matters in list index iteration.
+            # Start from the end
+            edges.pop(i)
+        for paper in tainted_papers:
+            try:
+                nodes.remove(paper)
+            except ValueError:
+                pass
     
-make_network_graph()
+    make_graph(nodes, edges)
+    plt.show()
+
+# def make_graphs_2_degree(degrees=2):
+#     nodes = nodes_with_dave
+#     edges = edges_with_dave
+
+#     for edge_pair in edges:
+        
+    
+#     make_graph(nodes, edges)
+#     plt.show()
+    
+    
+make_graphs_no_dave()
